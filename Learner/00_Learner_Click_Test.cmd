320048a1238c6f5c5bb2a7016168b5e15be9b497
nakagaway218 <dynakagawa@gmail.com>
2026-06-06 22:27:07 +0900

    Add Learner study helper

---
+++ Learner/00_Learner_Click_Test.cmd
@@ -0,0 +1,13 @@
+@echo off
+cd /d "%~dp0"
+echo click test started > learner_click_test_log.txt
+echo ========================================
+echo Learner click test
+echo ========================================
+echo.
+echo If you can see this window, double-click works.
+echo.
+echo A log file was created:
+echo learner_click_test_log.txt
+echo.
+pause
